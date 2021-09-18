b43e48fe7e92660866b1ac49ceca593ede3de722
Brice Yuuki Gibson <byg160030@utdallas.edu>
2021-09-18 02:08:28 -0500

    Tile Spawner

---
+++ Assets/Scripts/TileSpawner.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileSpawner : MonoBehaviour {
+
+	DungeonManager dungMan;
+
+	void Start () {
+		dungMan = FindObjectOfType<DungeonManager>();
+	}
+	
+	void OnDrawGizmos()
+    {
+		Gizmos.color = Color.white;
+		Gizmos.DrawCube(transform.position, Vector3.one);
+    }
+}
