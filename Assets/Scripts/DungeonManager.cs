ab30e9d452c5c0c09452a30938d383842f4f2b69
Brice Yuuki Gibson <byg160030@utdallas.edu>
2021-09-19 03:05:39 -0500

    Exit Doorway
    Player can now exit the dungeon to a new dungeon using the exit doorway

--- Assets/Scripts/DungeonManager.cs
+++ Assets/Scripts/DungeonManager.cs
@@ -5,9 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class DungeonManager : MonoBehaviour {
 
-	public GameObject floorPrefab, wallPrefab, tilePrefab;
+    public GameObject[] randomItems;
+    public GameObject floorPrefab, wallPrefab, tilePrefab, exitPrefab;
 
-	[HideInInspector] public float minX, maxX, minY, maxY;
+    [HideInInspector] public float minX, maxX, minY, maxY;
 
     private List<Vector3> floorList = new List<Vector3>();
     public int totalFloorCount;
@@ -19,7 +20,7 @@ public class DungeonManager : MonoBehaviour {
 
     void Update()
     {
-       if(Application.isEditor && Input.GetKeyDown(KeyCode.Backspace))
+        if (Application.isEditor && Input.GetKeyDown(KeyCode.Backspace))
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
@@ -32,17 +33,17 @@ public class DungeonManager : MonoBehaviour {
         floorList.Add(curPos);
         while (floorList.Count < totalFloorCount)
         {
-            switch(Random.Range(1, 5))
+            switch (Random.Range(1, 5))
             {
-                case 1: curPos += Vector3.up;   break;
-                case 2: curPos += Vector3.right;break;
+                case 1: curPos += Vector3.up; break;
+                case 2: curPos += Vector3.right; break;
                 case 3: curPos += Vector3.down; break;
                 case 4: curPos += Vector3.left; break;
             }
             bool inFloorList = false;
-            for(int i = 0; i < floorList.Count; i++)
+            for (int i = 0; i < floorList.Count; i++)
             {
-                if(Vector3.Equals(curPos, floorList[i]))
+                if (Vector3.Equals(curPos, floorList[i]))
                 {
                     inFloorList = true;
                     break;
@@ -53,11 +54,27 @@ public class DungeonManager : MonoBehaviour {
                 floorList.Add(curPos);
             }
         }
-        for(int i = 0; i < floorList.Count; i++)
+        for (int i = 0; i < floorList.Count; i++)
         {
             GameObject goTile = Instantiate(tilePrefab, floorList[i], Quaternion.identity) as GameObject;
             goTile.name = tilePrefab.name;
             goTile.transform.SetParent(transform);
         }
+        StartCoroutine(DelayProgress());
+    }
+    IEnumerator DelayProgress()
+    {
+        while (FindObjectsOfType<TileSpawner>().Length > 0)
+        {
+            yield return null;
+        }
+        ExitDoorway();
+    }
+    void ExitDoorway()
+    {
+        Vector3 doorPos = floorList[floorList.Count - 1];
+        GameObject goDoor = Instantiate(exitPrefab, doorPos, Quaternion.identity) as GameObject;
+        goDoor.name = exitPrefab.name;
+        goDoor.transform.SetParent(transform);
     }
 }
